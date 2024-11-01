ea5a8320e15cdda21366b735272655b039b340f1
TychoQS <tycho.quintana@gmail.com>
2024-11-01 01:35:19 +0000

    Basic model classes of the application implemented.

---
+++ src/main/java/software/ulpgc/model/Currency.java
@@ -0,0 +1,49 @@
+package software.ulpgc.model;
+
+import java.util.Objects;
+
+public class Currency {
+    private final String code;
+    private final String name;
+    private final String symbol;
+
+    public Currency(String code, String name, String symbol) {
+        this.code = code;
+        this.name = name;
+        this.symbol = symbol;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Currency money = (Currency) o;
+        return Objects.equals(code, money.code) && Objects.equals(name, money.name) && Objects.equals(symbol, money.symbol);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(code, name, symbol);
+    }
+
+    @Override
+    public String toString() {
+        return "Money{" +
+                "code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                ", symbol='" + symbol + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/software/ulpgc/model/ExchangeRate.java
@@ -0,0 +1,43 @@
+package software.ulpgc.model;
+
+import java.time.LocalDate;
+
+public class ExchangeRate {
+    private final LocalDate date;
+    private final double rate;
+    private final Currency from;
+    private final Currency to;
+
+    public ExchangeRate(LocalDate date, double rate, Currency from, Currency to) {
+        this.date = date;
+        this.rate = rate;
+        this.from = from;
+        this.to = to;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public Currency getFrom() {
+        return from;
+    }
+
+    public Currency getTo() {
+        return to;
+    }
+
+    @Override
+    public String toString() {
+        return "ExchangeRate{" +
+                "date=" + date +
+                ", rate=" + rate +
+                ", from=" + from +
+                ", to=" + to +
+                '}';
+    }
+}
