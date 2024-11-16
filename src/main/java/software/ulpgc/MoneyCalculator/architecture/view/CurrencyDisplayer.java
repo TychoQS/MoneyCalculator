00229bc166ed0b530f83ff4016880761c0909324
TychoQS <tycho.quintana@gmail.com>
2024-11-16 22:33:06 +0000

    Interfaces to display data on the GUI created as well as the view package of the architecture.

---
+++ src/main/java/software/ulpgc/MoneyCalculator/architecture/view/CurrencyDisplayer.java
@@ -0,0 +1,7 @@
+package software.ulpgc.MoneyCalculator.architecture.view;
+
+import java.util.Currency;
+
+public interface CurrencyDisplayer {
+    void display(Currency currency);
+}
---
+++ src/main/java/software/ulpgc/MoneyCalculator/architecture/view/MoneyDisplayer.java
@@ -0,0 +1,7 @@
+package software.ulpgc.MoneyCalculator.architecture.view;
+
+import software.ulpgc.MoneyCalculator.architecture.model.Money;
+
+public interface MoneyDisplayer {
+    void display(Money money);
+}
