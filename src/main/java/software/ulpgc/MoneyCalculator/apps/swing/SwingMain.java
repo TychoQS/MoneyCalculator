4881dbb33cc140f7dd0b0d7f7b1e8a73ec97f68d
TychoQS <tycho.quintana@gmail.com>
2024-11-20 01:29:11 +0000

    TODO

--- src/main/java/software/ulpgc/MoneyCalculator/apps/swing/SwingMain.java
+++ src/main/java/software/ulpgc/MoneyCalculator/apps/swing/SwingMain.java
@@ -4,7 +4,7 @@ public class SwingMain {
     public static void main(String[] args) {
         SwingMainFrame mainFrame = new SwingMainFrame();
         mainFrame.setVisible(true);
-        // TODO -> Implementar POJO para cuando traemos los datos del servicio web
+        // TODO -> Refactorizar CurrencyCodeToSymbol (Creo que como método estático de una clase queda mejor)
         // TODO -> Refactorizar para abrir los recursos perteneciente a la carpeta resources a como hemos visto en clase (Objeto .class)
     }
 }
