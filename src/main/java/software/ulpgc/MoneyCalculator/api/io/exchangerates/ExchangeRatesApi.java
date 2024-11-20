e05aed6c8158de543a748a212b23686833857fbe
TychoQS <tycho.quintana@gmail.com>
2024-11-20 00:01:45 +0000

    Added a new field to store the endpoint to get the

--- src/main/java/software/ulpgc/MoneyCalculator/api/io/exchangerates/ExchangeRatesApi.java
+++ src/main/java/software/ulpgc/MoneyCalculator/api/io/exchangerates/ExchangeRatesApi.java
@@ -2,5 +2,6 @@ package software.ulpgc.MoneyCalculator.api.io.exchangerates;
 
 public class ExchangeRatesApi {
     public static final String apiKey = "b8efb038c114944bf856016f761d424d";
+    public static final String getSymbolEndpoint = "http://api.exchangeratesapi.io/v1/symbols?access_key=";
 
 }
