a1653324178561ebdb48bb0529ae61da6f87306c
TychoQS <tycho.quintana@gmail.com>
2024-11-20 00:12:24 +0000

    Fixed Pojo to match endpoint response

--- src/main/java/software/ulpgc/MoneyCalculator/api/io/pojos/ExchangeRatesSymbolsGetResponse.java
+++ src/main/java/software/ulpgc/MoneyCalculator/api/io/pojos/ExchangeRatesSymbolsGetResponse.java
@@ -3,7 +3,9 @@ package software.ulpgc.MoneyCalculator.api.io.pojos;
 import java.util.List;
 import java.util.Map;
 
-public record ExchangeRatesSymbolsGetResponse(List<Symbol> symbols) {
+public record ExchangeRatesSymbolsGetResponse(Success success, List<Symbol> symbols) {
     public record Symbol(Map<String, String> response) {
+    }
+    public record Success (boolean value) {
     }
 }
