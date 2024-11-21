b50ed0fa85ced72ef476bf9fa65de0083892f491
TychoQS <tycho.quintana@gmail.com>
2024-11-21 00:15:01 +0000

    Implementation now is more divided.

---
+++ src/main/java/software/ulpgc/MoneyCalculator/architecture/io/FileCurrencyCodeToSymbolListLoader.java
@@ -0,0 +1,49 @@
+package software.ulpgc.MoneyCalculator.architecture.io;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FileCurrencyCodeToSymbolListLoader implements CurrencyCodeToSymbolLoader {
+    public static final int CODE = 0;
+    public static final int SYMBOL = 1;
+    private final codeAndSymbolDeserializer deserializer;
+    private final File file;
+
+    public FileCurrencyCodeToSymbolListLoader(File file, codeAndSymbolDeserializer deserializer) {
+        this.file = file;
+        this.deserializer = deserializer;
+    }
+
+    @Override
+    public CurrencyCodeToSymbol load() throws IOException {
+        return new CurrencyCodeToSymbol(getCurrencyCodeToSymbolMap());
+    }
+
+    private Map<String, String> getCurrencyCodeToSymbolMap() throws IOException {
+        Map<String, String> codesToSymbols = new HashMap<>();
+        try (BufferedReader reader = getBufferedReader()) {
+            reader.readLine();
+            while (true) {
+                String line = reader.readLine();
+                if (line == null) break;
+                codesToSymbols.put(getKey(deserializer.deserialize(line)), getValue(deserializer.deserialize(line)));
+            }
+        }
+        return codesToSymbols;
+    }
+
+    private String getValue(List<String> codeAndSymbol) {
+        return codeAndSymbol.get(SYMBOL);
+    }
+
+    private String getKey(List<String> codeAndSymbol) {
+        return codeAndSymbol.get(CODE);
+    }
+
+    private BufferedReader getBufferedReader() throws IOException {
+        return new BufferedReader(new InputStreamReader(Files.newInputStream(file.toPath())));
+    }
+}
