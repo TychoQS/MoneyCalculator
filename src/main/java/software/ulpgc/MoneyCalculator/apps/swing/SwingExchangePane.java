ba15c7c6dec2b8e80d2db48d6dc17f3181fd0406
TychoQS <tycho.quintana@gmail.com>
2024-11-21 02:14:22 +0000

    Added SwingExchangePane

---
+++ src/main/java/software/ulpgc/MoneyCalculator/apps/swing/SwingExchangePane.java
@@ -0,0 +1,49 @@
+package software.ulpgc.MoneyCalculator.apps.swing;
+
+import software.ulpgc.MoneyCalculator.architecture.model.Currency;
+import software.ulpgc.MoneyCalculator.architecture.view.CurrenciesDisplay;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+
+public class SwingExchangePane {
+
+    private final JPanel pane;
+    private final CurrenciesDisplay fromCurrency;
+    private final CurrenciesDisplay toCurrency;
+    private final List<Currency> currencies;
+
+    public SwingExchangePane(List<Currency> currencies) {
+        this.currencies = currencies;
+        pane = new JPanel(new BorderLayout());
+        fromCurrency = new SwingCurrenciesDisplay();
+        toCurrency = new SwingCurrenciesDisplay();
+        pane.add(BorderLayout.CENTER, comboBoxPane());
+        pane.add(BorderLayout.SOUTH, exchangeButton());
+        // TODO Probar con BorderLayoutRecursivos
+    }
+
+    private Component comboBoxPane() {
+        JPanel comboBoxPane = new JPanel();
+        comboBoxPane.add((Component) fromCurrency);
+        comboBoxPane.add((Component) toCurrency);
+        return comboBoxPane;
+    }
+
+    private Component textField() {
+        return new JLabel("Adios");
+    }
+
+    private Component exchangeButton() {
+        JButton button = new JButton("exchange");
+        button.addActionListener(e -> System.out.println("EXCHANGE BUTTON MOCK IMPLEMENTATION"));
+        return button;
+    }
+
+    public JPanel create() {
+        fromCurrency.display(currencies);
+        toCurrency.display(currencies);
+        return pane;
+    }
+}
