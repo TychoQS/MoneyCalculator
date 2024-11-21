f49ed754e243cd6bcf59f908a4680bb0da58c3b5
TychoQS <tycho.quintana@gmail.com>
2024-11-21 23:37:14 +0000

    Added getters to access to Dialogs and changed the Layout of the centerPane to fix the desired layout.

--- src/main/java/software/ulpgc/MoneyCalculator/apps/swing/SwingMainFrame.java
+++ src/main/java/software/ulpgc/MoneyCalculator/apps/swing/SwingMainFrame.java
@@ -1,6 +1,8 @@
 package software.ulpgc.MoneyCalculator.apps.swing;
 
 import software.ulpgc.MoneyCalculator.architecture.model.Currency;
+import software.ulpgc.MoneyCalculator.architecture.view.CurrenciesDialog;
+import software.ulpgc.MoneyCalculator.architecture.view.MoneyDialog;
 
 import javax.swing.*;
 import java.awt.*;
@@ -9,6 +11,8 @@ import java.util.List;
 public class SwingMainFrame extends JFrame {
 
     private final List<Currency> currencies;
+    private SwingCurrenciesDialogsPane swingCurrenciesDialogsPane;
+    private SwingConversionPane swingConversionPane;
 
     public SwingMainFrame(List<Currency> currencies) throws HeadlessException {
         super();
@@ -16,7 +20,20 @@ public class SwingMainFrame extends JFrame {
         initFrame();
         this.add(BorderLayout.NORTH, titlePane());
         this.add(BorderLayout.CENTER, centerPane());
+
         // TODO -> Añadir el resto de componentes para la visualización básica
+    }
+
+    public CurrenciesDialog getFromCurrency() {
+        return swingCurrenciesDialogsPane.getFromCurrency();
+    }
+
+    public CurrenciesDialog getToCurrency() {
+        return swingCurrenciesDialogsPane.getToCurrency();
+    }
+
+    public MoneyDialog getMoneyDialog() {
+        return swingConversionPane.getMoneyDialog();
     }
 
     private void initFrame() {
@@ -30,13 +47,19 @@ public class SwingMainFrame extends JFrame {
 
     private Component centerPane() {
         JPanel centerPane = new JPanel();
-        centerPane.setLayout(new BoxLayout(centerPane, BoxLayout.Y_AXIS));
-        centerPane.add(currenciesDialogPane());
+        centerPane.setLayout(new BorderLayout());
+        centerPane.add(BorderLayout.NORTH, currenciesDialogPane());
+        centerPane.add(BorderLayout.CENTER, conversionPane());
         return centerPane;
+    }
+    private Component conversionPane() {
+        swingConversionPane = new SwingConversionPane(getFromCurrency());
+        return swingConversionPane.getConversionPane();
     }
 
     private Component currenciesDialogPane() {
-        return new SwingCurrenciesDialogsPane(currencies).getCurrenciesDialogsPane();
+        swingCurrenciesDialogsPane = new SwingCurrenciesDialogsPane(currencies);
+        return swingCurrenciesDialogsPane.getCurrenciesDialogsPane();
     }
 
     private Component titlePane() {
