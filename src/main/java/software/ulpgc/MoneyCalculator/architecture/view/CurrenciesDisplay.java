fc5680cd8807c5055689624cb5aea236eb4bd4c5
TychoQS <tycho.quintana@gmail.com>
2024-11-21 01:12:18 +0000

    CurrenciesDisplay to display multiple currencies created

---
+++ src/main/java/software/ulpgc/MoneyCalculator/architecture/view/CurrenciesDisplay.java
@@ -0,0 +1,9 @@
+package software.ulpgc.MoneyCalculator.architecture.view;
+
+import software.ulpgc.MoneyCalculator.architecture.model.Currency;
+
+import java.util.List;
+
+public interface CurrenciesDisplay {
+    void display(List<Currency> currencies);
+}
