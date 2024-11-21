7ef04a41e336ae477f5ac2330ddeab5dbfd54409
TychoQS <tycho.quintana@gmail.com>
2024-11-21 22:15:47 +0000

    Changes made to have the desired alignment of panels.

--- src/main/java/software/ulpgc/MoneyCalculator/apps/swing/SwingMainFrame.java
+++ src/main/java/software/ulpgc/MoneyCalculator/apps/swing/SwingMainFrame.java
@@ -2,6 +2,7 @@ package software.ulpgc.MoneyCalculator.apps.swing;
 
 import software.ulpgc.MoneyCalculator.architecture.model.Currency;
 import software.ulpgc.MoneyCalculator.architecture.view.CurrencyDisplay;
+import software.ulpgc.MoneyCalculator.mock.swing.SwingMockPanel;
 
 import javax.swing.*;
 import java.awt.*;
@@ -12,16 +13,24 @@ public class SwingMainFrame extends JFrame {
     private final List<Currency> currencies;
 
     public SwingMainFrame(List<Currency> currencies) throws HeadlessException {
+        super();
         this.currencies = currencies;
         this.setTitle("Money Calculator");
         this.setSize(800, 800);
         this.setResizable(false);
         this.setLocationRelativeTo(null);
-        this.setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
+        this.setLayout(new BorderLayout());
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.add(titlePane());
-        this.add(exchangePane());
+        this.add(BorderLayout.NORTH, titlePane());
+        this.add(BorderLayout.CENTER, centerPane());
         // TODO -> Añadir el resto de componentes para la visualización básica
+    }
+
+    private Component centerPane() {
+        JPanel centerPane = new JPanel();
+        centerPane.setLayout(new BoxLayout(centerPane, BoxLayout.Y_AXIS));
+        centerPane.add(exchangePane());
+        return centerPane;
     }
 
     private Component exchangePane() {
