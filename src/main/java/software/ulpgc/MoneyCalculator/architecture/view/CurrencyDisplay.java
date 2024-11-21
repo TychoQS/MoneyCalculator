2801bddd5d5d4fb35ffbe4fb28e848de4db1678e
TychoQS <tycho.quintana@gmail.com>
2024-11-21 22:29:19 +0000

    Not being used, and not planned to used it.

--- src/main/java/software/ulpgc/MoneyCalculator/architecture/view/CurrencyDisplay.java
+++
@@ -1,7 +0,0 @@
-package software.ulpgc.MoneyCalculator.architecture.view;
-
-import software.ulpgc.MoneyCalculator.architecture.model.Currency;
-
-public interface CurrencyDisplay {
-    void display(Currency currency);
-}
