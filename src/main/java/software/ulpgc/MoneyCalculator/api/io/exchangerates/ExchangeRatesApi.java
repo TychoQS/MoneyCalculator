a38f075ab181876bce70bd1fcb979879d957b16a
TychoQS <tycho.quintana@gmail.com>
2024-11-24 17:58:51 +0000

    Refactorized

--- src/main/java/software/ulpgc/MoneyCalculator/api/io/exchangerates/ExchangeRatesApi.java
+++ src/main/java/software/ulpgc/MoneyCalculator/api/io/exchangerates/ExchangeRatesApi.java
@@ -2,6 +2,5 @@ package software.ulpgc.MoneyCalculator.api.io.exchangerates;
 
 public class ExchangeRatesApi {
     public static final String apiKey = "b8efb038c114944bf856016f761d424d";
-    public static final String getSymbolEndpoint = "http://api.exchangeratesapi.io/v1/symbols?access_key=";
-
+    public static final String SymbolEndpoint = "http://api.exchangeratesapi.io/v1/symbols?access_key=";
 }
