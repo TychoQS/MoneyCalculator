76a552ef9c59efc041fc32c175a42ac77f23e195
TychoQS <tycho.quintana@gmail.com>
2024-11-24 19:25:49 +0000

    Change wrong pojo

--- src/main/java/software/ulpgc/MoneyCalculator/api/io/currencylayer/CurrencyLayerExchangeRateDeserializer.java
+++ src/main/java/software/ulpgc/MoneyCalculator/api/io/currencylayer/CurrencyLayerExchangeRateDeserializer.java
@@ -1,11 +1,12 @@
 package software.ulpgc.MoneyCalculator.api.io.currencylayer;
 
 import com.google.gson.Gson;
-import software.ulpgc.MoneyCalculator.api.io.pojos.ExchangeRatesSymbolsGetResponse;
+import software.ulpgc.MoneyCalculator.api.io.pojos.CurrencyLayerExchangeRateGetResponse;
+import software.ulpgc.MoneyCalculator.architecture.io.ExchangeRateDeserializer;
 
-public class CurrencyLayerExchangeRateDeserializer implements ExchangeRateDeserializer{
+public class CurrencyLayerExchangeRateDeserializer implements ExchangeRateDeserializer {
     @Override
     public Object deserialize(String read) {
-        return new Gson().fromJson(read, ExchangeRatesSymbolsGetResponse.class);
+        return new Gson().fromJson(read, CurrencyLayerExchangeRateGetResponse.class);
     }
 }
