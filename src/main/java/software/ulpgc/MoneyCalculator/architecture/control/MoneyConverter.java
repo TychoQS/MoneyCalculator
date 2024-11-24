1d43ac00633b09f966abf66780bb5cf5c4dd3233
TychoQS <tycho.quintana@gmail.com>
2024-11-24 21:04:51 +0000

    Removed console write

--- src/main/java/software/ulpgc/MoneyCalculator/architecture/control/MoneyConverter.java
+++ src/main/java/software/ulpgc/MoneyCalculator/architecture/control/MoneyConverter.java
@@ -5,8 +5,6 @@ import software.ulpgc.MoneyCalculator.architecture.model.Money;
 
 public class MoneyConverter {
     public static Money convert(Money money, ExchangeRate exchangeRate) {
-        System.out.println("Exchange rate: " + exchangeRate);
         return new Money(money.getAmount() * exchangeRate.getRate(), exchangeRate.getTo());
     }
-
 }
