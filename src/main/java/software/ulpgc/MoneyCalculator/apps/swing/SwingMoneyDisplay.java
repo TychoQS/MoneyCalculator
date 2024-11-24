cc68fb9dbcbbd60c0076839727dcf66ac71a7e6b
TychoQS <tycho.quintana@gmail.com>
2024-11-24 21:30:25 +0000

    Now a custom font is used for the label.

--- src/main/java/software/ulpgc/MoneyCalculator/apps/swing/SwingMoneyDisplay.java
+++ src/main/java/software/ulpgc/MoneyCalculator/apps/swing/SwingMoneyDisplay.java
@@ -4,10 +4,19 @@ import software.ulpgc.MoneyCalculator.architecture.model.Money;
 import software.ulpgc.MoneyCalculator.architecture.view.MoneyDisplay;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class SwingMoneyDisplay extends JLabel implements MoneyDisplay {
+
+    public static final int FONT_SIZE = 15;
+
     public SwingMoneyDisplay() {
         super();
+        this.setFont(this.setFont());
+    }
+
+    private Font setFont() {
+        return new Font("Arial", Font.PLAIN, FONT_SIZE);
     }
 
     @Override
