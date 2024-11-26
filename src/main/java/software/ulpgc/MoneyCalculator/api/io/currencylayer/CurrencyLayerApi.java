8a14c6e971ee55175f410101781724550cf9f9f3
TychoQS <prueba@pruebas.com>
2024-11-26 17:13:14 +0000

    Changed API_KEY.

--- src/main/java/software/ulpgc/MoneyCalculator/api/io/currencylayer/CurrencyLayerApi.java
+++ src/main/java/software/ulpgc/MoneyCalculator/api/io/currencylayer/CurrencyLayerApi.java
@@ -1,7 +1,7 @@
 package software.ulpgc.MoneyCalculator.api.io.currencylayer;
 
 public class CurrencyLayerApi {
-    public static String apiKey= "f508c1f6d1c6705e296fec36513ba0ab&source";
+    public static String apiKey= "d103d311647949d609482f4c93f62012";
     private static String ExchangeRateEndpoint = "https://api.currencylayer.com/live?access_key=%s&source=%s&currencies=%s";
 
     public static String getExchangeRateEndpoint(String fromCurrency, String toCurrency) {
