dc46ea91232da03049a2bc5137cb7b5107f8b2ef
TychoQS <prueba@pruebas.com>
2024-11-26 18:05:17 +0000

    Added TODO

--- src/main/java/software/ulpgc/MoneyCalculator/architecture/control/Command.java
+++ src/main/java/software/ulpgc/MoneyCalculator/architecture/control/Command.java
@@ -2,6 +2,6 @@ package software.ulpgc.MoneyCalculator.architecture.control;
 
 import java.io.IOException;
 
-public interface Command {
+public interface Command { //TODO -> Look for IOException source
     void execute() throws IOException;
 }
