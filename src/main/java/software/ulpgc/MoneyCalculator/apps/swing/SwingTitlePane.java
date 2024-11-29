ac29d605ff0b61d98fcf239ff4572480496ccb11
TychoQS <prueba@pruebas.com>
2024-11-29 14:57:41 +0000

    Changed font

--- src/main/java/software/ulpgc/MoneyCalculator/apps/swing/SwingTitlePane.java
+++ src/main/java/software/ulpgc/MoneyCalculator/apps/swing/SwingTitlePane.java
@@ -6,11 +6,8 @@ import java.awt.*;
 public class SwingTitlePane {
     public static final Color LABEL_COLOR = Color.BLACK;
     public static final String LABEL_TEXT = "Money Calculator";
-    private static final int LABEL_PREFERRED_WIDTH = 300;
-    private static final int LABEL_PREFERRED_HEIGHT = 60;
-    private static final String FONT_NAME = "Power Green Narrow";
+    private static final String FONT_NAME = "Agency FB";
     private final JPanel titlePane;
-    private JLabel titleLabel;
 
     public SwingTitlePane() {
         titlePane = new JPanel();
@@ -18,9 +15,8 @@ public class SwingTitlePane {
     }
 
     private Component getTitleLabel() {
-        this.titleLabel = new JLabel(LABEL_TEXT);
+        JLabel titleLabel = new JLabel(LABEL_TEXT);
         titleLabel.setFont(getFont());
-        titleLabel.setPreferredSize(getPreferredSize());
         titleLabel.setForeground(LABEL_COLOR);
         return titleLabel;
     }
@@ -29,15 +25,11 @@ public class SwingTitlePane {
         return new Font(FONT_NAME, Font.PLAIN, adaptFontSize());
     }
 
-    private Dimension getPreferredSize() {
-        return new Dimension(LABEL_PREFERRED_WIDTH, LABEL_PREFERRED_HEIGHT);
-    }
-
     private int adaptFontSize() {
         return Toolkit.getDefaultToolkit().getScreenSize().width / 50;
     }
 
     public JPanel getTitlePane() {
-        return titlePane;
+        return this.titlePane;
     }
 }
