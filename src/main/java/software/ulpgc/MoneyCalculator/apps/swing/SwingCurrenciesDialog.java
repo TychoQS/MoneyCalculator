7ee967baa730e2c3739709ba60dc32d39d6c3edb
TychoQS <tycho.quintana@gmail.com>
2024-12-01 20:13:07 +0000

    Added font to CurrenciesDisplay

--- src/main/java/software/ulpgc/MoneyCalculator/apps/swing/SwingCurrenciesDialog.java
+++ src/main/java/software/ulpgc/MoneyCalculator/apps/swing/SwingCurrenciesDialog.java
@@ -4,12 +4,16 @@ import software.ulpgc.MoneyCalculator.architecture.model.Currency;
 import software.ulpgc.MoneyCalculator.architecture.view.CurrenciesDialog;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.List;
 
 public class SwingCurrenciesDialog extends JComboBox implements CurrenciesDialog {
 
+    public static final Font FONT = new Font("FB Agent", Font.PLAIN, 15);
+
     public SwingCurrenciesDialog() {
         super();
+        this.setFont(FONT);
     }
 
     @Override
