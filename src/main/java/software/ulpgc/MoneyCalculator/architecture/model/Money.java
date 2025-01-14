e45c2ede56b8222b639a5fd81ba58e3a09919983
TychoQS <tycho.quintana@gmail.com>
2025-01-14 22:40:31 +0000

    Converted to record

--- src/main/java/software/ulpgc/MoneyCalculator/architecture/model/Money.java
+++ src/main/java/software/ulpgc/MoneyCalculator/architecture/model/Money.java
@@ -1,24 +1,9 @@
 package software.ulpgc.MoneyCalculator.architecture.model;
 
-public class Money {
-    private final double amount;
-    private final Currency currency;
-
-    public Money(double amount, Currency currency) {
-        this.amount = amount;
-        this.currency = currency;
-    }
+public record Money(double amount, Currency currency) {
 
     public static Money getFrom(double amount, Currency currency) {
         return new Money(amount, currency);
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public Currency getCurrency() {
-        return currency;
     }
 
     @Override
