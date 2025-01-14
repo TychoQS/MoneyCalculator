d4b30a22f084a099e8db08b850a1fd13e2303d90
TychoQS <tycho.quintana@gmail.com>
2025-01-14 22:45:16 +0000

    Removed unused methods

--- src/main/java/software/ulpgc/MoneyCalculator/architecture/model/ExchangeRate.java
+++ src/main/java/software/ulpgc/MoneyCalculator/architecture/model/ExchangeRate.java
@@ -23,14 +23,6 @@ public class ExchangeRate {
         return rate;
     }
 
-    public Currency getFrom() {
-        return from;
-    }
-
-    public Currency getTo() {
-        return to;
-    }
-
     @Override
     public String toString() {
         return "ExchangeRate{" +
