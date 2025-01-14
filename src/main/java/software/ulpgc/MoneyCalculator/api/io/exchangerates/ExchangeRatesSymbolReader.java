c137aba075fede0bac1c194641d0acaee1e76fb2
TychoQS <tycho.quintana@gmail.com>
2025-01-14 22:44:29 +0000

    Refactor

--- src/main/java/software/ulpgc/MoneyCalculator/api/io/exchangerates/ExchangeRatesSymbolReader.java
+++ src/main/java/software/ulpgc/MoneyCalculator/api/io/exchangerates/ExchangeRatesSymbolReader.java
@@ -8,22 +8,23 @@ import java.io.IOException;
 import static org.jsoup.Connection.Method.GET;
 
 public class ExchangeRatesSymbolReader implements SymbolReader {
+
+    public static final String ACCEPT = "accept";
+    public static final String ALL_TEXT = "text/*";
+    public static final int HTTP_OK = 200;
+
     @Override
     public String read() {
         try {
-            return read(ExchangeRatesApi.SymbolEndpoint + ExchangeRatesApi.apiKey);
+            Connection.Response response = Jsoup.connect(ExchangeRatesApi.SymbolEndpoint + ExchangeRatesApi.apiKey)
+                    .ignoreContentType(true)
+                    .header(ACCEPT, ALL_TEXT)
+                    .method(GET)
+                    .execute();
+            if (response.statusCode() != HTTP_OK) throw new RuntimeException();
+            return response.body();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    private String read(String endpointUrl) throws IOException {
-        Connection.Response response = Jsoup.connect(endpointUrl)
-                .ignoreContentType(true)
-                .header("accept", "text/*")
-                .method(GET)
-                .execute();
-        if (response.statusCode() != 200) throw new RuntimeException();
-        return response.body();
     }
 }
