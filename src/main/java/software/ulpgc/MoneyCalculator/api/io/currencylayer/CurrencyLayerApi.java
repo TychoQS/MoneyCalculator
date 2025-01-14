aeee37b2f1da7bc96916f2e845555bed88d23326
TychoQS <tycho.quintana@gmail.com>
2025-01-14 21:11:16 +0000

    Changed APIKey and removed main

--- src/main/java/software/ulpgc/MoneyCalculator/api/io/currencylayer/CurrencyLayerApi.java
+++ src/main/java/software/ulpgc/MoneyCalculator/api/io/currencylayer/CurrencyLayerApi.java
@@ -1,7 +1,7 @@
 package software.ulpgc.MoneyCalculator.api.io.currencylayer;
 
 public class CurrencyLayerApi {
-    public static final String API_KEY = "cd5721d509fd5b1c1603f774ad38c022";
+    public static final String API_KEY = "d103d311647949d609482f4c93f62012";
     private static final String EXCHANGE_RATE_ENDPOINT = "https://api.currencylayer.com/live?access_key=%s&source=%s&currencies=%s";
     private static final String DATE_CONVERSION_ENDPOINT = "https://api.currencylayer.com/convert?access_key=%s&from=%s&to=%s&amount=1&date=%s";
 
@@ -11,10 +11,5 @@ public class CurrencyLayerApi {
 
     public static String getDateConversionEndpoint(String fromCurrency, String toCurrency, String date) {
         return String.format(DATE_CONVERSION_ENDPOINT, API_KEY, fromCurrency, toCurrency, date);
-    }
-
-    public static void main(String[] args) { // TODO -> Remove
-        System.out.println(CurrencyLayerApi.getExchangeRateEndpoint("USD", "EUR"));
-        System.out.println(CurrencyLayerApi.getDateConversionEndpoint("AOA", "ARS", "2024-12-27"));
     }
 }
