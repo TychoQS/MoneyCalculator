2309c77c82575a840af1a9931337e8c9fe889832
TychoQS <tycho.quintana@gmail.com>
2025-01-14 22:43:53 +0000

    Refactor

--- src/main/java/software/ulpgc/MoneyCalculator/api/io/pojos/ExchangeRatesSymbolsGetResponse.java
+++ src/main/java/software/ulpgc/MoneyCalculator/api/io/pojos/ExchangeRatesSymbolsGetResponse.java
@@ -2,6 +2,4 @@ package software.ulpgc.MoneyCalculator.api.io.pojos;
 
 import java.util.Map;
 
-public record ExchangeRatesSymbolsGetResponse(boolean success, Map<String, String> symbols) {
-
-}
+public record ExchangeRatesSymbolsGetResponse(boolean success, Map<String, String> symbols) {}
